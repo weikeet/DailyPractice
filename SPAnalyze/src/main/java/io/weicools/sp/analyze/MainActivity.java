9aa260212df6ba9b763e7d9a953431a11c057d85
zhengwei.zhang <zhengwei.zhang@ihandysoft.com>
2019-01-16 09:41:49 +0800

    sp sample

--- SPAnalyze/src/main/java/io/weicools/sp/analyze/MainActivity.java
+++ SPAnalyze/src/main/java/io/weicools/sp/analyze/MainActivity.java
@@ -1,5 +1,7 @@
 package io.weicools.sp.analyze;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate (Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    SharedPreferences sp = getSharedPreferences("settings", Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = sp.edit();
+    editor.putString("hello", "world");
+    editor.putInt("goal", 3000);
+    editor.apply();
   }
 }
