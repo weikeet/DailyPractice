54c43174f4b80b60da84cb2fce8963e38fe59789
zhengwei.zhang <zhengwei.zhang@ihandysoft.com>
2019-03-16 19:35:22 +0800

    test TabLayout app:tabIndicatorFullWidth="false"

---
+++ app/src/main/java/io/weicools/daily/practice/TabTestActivity.java
@@ -0,0 +1,18 @@
+package io.weicools.daily.practice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Weicools Create on 2019.03.16
+ *
+ * desc:
+ */
+public class TabTestActivity extends AppCompatActivity {
+  @Override
+  protected void onCreate (@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_tab_test);
+  }
+}
